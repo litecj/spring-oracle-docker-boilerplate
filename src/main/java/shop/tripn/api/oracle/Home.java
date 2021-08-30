e3319a1100b3861a5b0c2467b30422251676278b
litecj <litecj702@gmail.com>
2021-08-30 14:47:36 +0900

    first commit

---
+++ src/main/java/shop/tripn/api/oracle/Home.java
@@ -0,0 +1,14 @@
+package shop.tripn.api.oracle;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@RestController
+public class Home {
+    @GetMapping("/")
+    public String home(){
+        return new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date());
+    }
+}
