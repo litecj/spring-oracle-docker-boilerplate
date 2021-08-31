94ba3bf949c1759d8b003c8961d8c48672640bac
litecj <litecj702@gmail.com>
2021-08-31 17:50:25 +0900

    end

--- src/main/java/shop/jarviis/api/oracle/HomeController.java
+++ src/main/java/shop/jarviis/api/oracle/HomeController.java
@@ -1,14 +1,17 @@
-package shop.tripn.api.oracle;
+package shop.jarviis.api.oracle;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.xml.crypto.Data;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @RestController
-public class Home {
+public class HomeController {
     @GetMapping("/")
-    public String home(){
+    public String home() {
         return new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date());
     }
 }
--- src/main/java/shop/jarviis/api/oracle/OracleApplication.java
+++ src/main/java/shop/jarviis/api/oracle/OracleApplication.java
@@ -1,4 +1,4 @@
-package shop.tripn.api.oracle;
+package shop.jarviis.api.oracle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
